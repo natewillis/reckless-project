6ef7fdfaee17c2c608565bef95c6bb0034ccace9
Nate <natewillis1@gmail.com>
2022-01-17 14:11:08 -0600

    added production/dev settings

--- reckless/reckless/settings/base.py
+++ reckless/reckless/settings/base.py
@@ -12,20 +12,25 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# imports for loading config
+import os
+import environ
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# load config
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^6hw1nzuve5u5$3&55##zcl!v#7mo9d-cm%06ufjb2yvywxtot'
+SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
