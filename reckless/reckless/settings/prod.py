f7e7cca6be9f8dd0778dbc473aa2539756ba4d44
Nate <natewillis1@gmail.com>
2022-01-18 09:24:48 -0600

    updating settings for deployment

--- reckless/reckless/settings/prod.py
+++ reckless/reckless/settings/prod.py
@@ -2,4 +2,7 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['recklessanalysis.com','www.recklessanalysis.com',]
+ALLOWED_HOSTS = ['recklessanalysis.com','www.recklessanalysis.com', '137.184.98.238', 'localhost']
+
+# STATIC FILES
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
